3df124056bdf2e36a284dd362037356e941ff76e
Kasib <kasibabdullah8@gmail.com>
2023-03-11 18:10:13 -0500

    fixed error in test, now checks for signup

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/wines/i);
+  const linkElement = screen.getByText('signup');
   expect(linkElement).toBeInTheDocument();
 });
