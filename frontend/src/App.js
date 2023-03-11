f07d5b24eedd4759c2f515ee79543d1299bd0f2a
Kasib <kasibabdullah8@gmail.com>
2023-03-11 17:56:24 -0500

    add bootstrap sign up form, made link to signup at homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,18 @@ import './App.css';
 import { HashRouter, Routes, Route, Link } from 'react-router-dom'
 import ListWines from "./components/ListWines"
 import CreateWine from "./components/CreateWine"
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
 
       <HashRouter>
-      <h1>Notional Nav barzz</h1>
-      <Link to="/all-wines">List Wines</Link>
+      <Link to="/signup">Signup</Link>
       <br></br>
-      <Link to="/create">Create Wine</Link>
         <Routes>
-          <Route path="/all-wines" element={ <ListWines />} />
-          <Route path="/create" element={ <CreateWine />} />
+          <Route path='/signup' element={<Signup />} />
+          
         </Routes>
       </HashRouter>
      
