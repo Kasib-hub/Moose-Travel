e2e6bc2bc34e9fad98a355bb920226b69f0b3ca3
Kasib <kasibabdullah8@gmail.com>
2023-03-13 22:00:07 -0400

    JWT implementation; access token stores userID and username

--- backend/moose_api/urls.py
+++ backend/moose_api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+
+#from the simpleJWT authentication
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', views.getRoutes)
+    path('', views.getRoutes),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), #takes in the username and password to give an authentication and refresh token
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #takes in the refresh token -> gives a new refresh token
 ]
 
 # urlpatterns = [
