427bc0fb09281d9cd8d6148283cd95f24551328d
Austin Grabarkewitz <agrabark@gmail.com>
2023-03-13 20:22:36 -0400

    "Created a Travel Agent AI"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,44 @@ import './App.css';
 import { HashRouter, Routes, Route, Link } from 'react-router-dom'
 import ListWines from "./components/ListWines"
 import CreateWine from "./components/CreateWine"
+import { useEffect } from 'react';
+import Chatbot from './components/Chatbot';
+
+const apiKey = 'prtl6749387986743898559646983194';
+const origin = 'ATL-sky'; // Atlanta airport code
+const destination = 'NYCA-sky'; // New York City airport code
+const date = new Date().toISOString().slice(0, 10); // today's date in ISO format (YYYY-MM-DD)
+
+
+
+// const getFlights = () => {
+
+//   fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${origin}/${destination}/${date}`, {
+//   headers: {
+//     'x-api-key': apiKey
+//   }
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+//     // Handle the response data here
+//   })
+//   .catch(error => {
+//     console.error(error);
+//     // Handle any errors here
+//   });
+
+// }
+
+// console.log("Flight Data: ");
+// getFlights();
+
 
 function App() {
   return (
     <div className="App">
 
-      <HashRouter>
+      {/* <HashRouter>
       <h1>Notional Nav barzz</h1>
       <Link to="/all-wines">List Wines</Link>
       <br></br>
@@ -16,8 +48,10 @@ function App() {
           <Route path="/all-wines" element={ <ListWines />} />
           <Route path="/create" element={ <CreateWine />} />
         </Routes>
-      </HashRouter>
+      </HashRouter> */}
      
+      <Chatbot/>
+
     </div>
   );
 }
