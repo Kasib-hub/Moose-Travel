88283ffb1dbe6c3f86681cc7460bd70b2af4cdbc
Kasib <kasibabdullah8@gmail.com>
2023-03-15 18:24:49 -0400

    removed another unused component import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import PrivateRoutes from './utils/PrivateRoutes';
 import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
