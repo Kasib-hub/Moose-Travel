3281b99ca59887355082b458b601e8fbec2996eb
Kasib <kasibabdullah8@gmail.com>
2023-03-15 19:14:03 -0400

    changed signup to signuppage needed to reflect in test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Signup');
+  const linkElement = screen.getByText('SignUpPage');
   expect(linkElement).toBeInTheDocument();
 });
