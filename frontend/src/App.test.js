26b14cf39b8f4d970997c15ac528d391b1a0f4dd
Kasib <kasibabdullah8@gmail.com>
2023-03-15 19:21:10 -0400

    added test for sign up button and page

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import SignUpPage from './pages/SignUpPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('SignUpPage');
-  expect(linkElement).toBeInTheDocument();
+describe('SignUpPage', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(<SignUpPage />);
+
+    expect(getByText('Sign Up')).toBeInTheDocument();
+  });
+
+  it('handles click event', () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(<SignUpPage onClick={handleClick} />);
+    const button = getByText('Sign Up');
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
 });
