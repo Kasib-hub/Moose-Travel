347b03dd3426eec4d60d4ab3fa031aeb9c3643d3
Kasib <kasibabdullah8@gmail.com>
2023-03-15 19:29:24 -0400

    added complexity to test, may need to remove

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,17 +1,25 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 
 describe('SignUpPage', () => {
   it('renders correctly', () => {
-    const { getByText } = render(<SignUpPage />);
+    const { getByText } 
+    = render(
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<SignUpPage />}/>
+        </Routes>
+      </BrowserRouter>
+    );
 
     expect(getByText('Sign Up')).toBeInTheDocument();
   });
 
   it('handles click event', () => {
     const handleClick = jest.fn();
-    const { getByText } = render(<SignUpPage onClick={handleClick} />);
+    const { getByText } = render(<SignUpPage handleSubmit={handleClick} />);
     const button = getByText('Sign Up');
 
     fireEvent.click(button);
