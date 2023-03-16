71e5ef26438dfd8fe2c2dc85706ab3af17967171
Hui Zhao <huizhao1003@gmail.com>
2023-03-16 16:47:16 -0400

    REST APIs done

--- backend/moose_api/serializers.py
+++ backend/moose_api/serializers.py
@@ -1,1 +1,103 @@
+from rest_framework import serializers
+from .models import *
+
+class ItinerarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Itinerary
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Itinerary.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.itinerary_name = validated_data.get('itinerary_name', instance.itinerary_name)
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.save()
+        return instance
+    
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Hotel.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.itinerary_id = validated_data.get('itinerary_id', instance.itinerary_id)
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.hotel_name = validated_data.get('hotel_name', instance.hotel_name)
+        instance.location = validated_data.get('location', instance.location)
+        instance.check_in_date = validated_data.get('check_in_date', instance.check_in_date)
+        instance.check_out_date= validated_data.get('check_out_date', instance.check_out_date)
+        instance.save()
+        return instance
+    
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Flight.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.itinerary_id = validated_data.get('itinerary_id', instance.itinerary_id)
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.flight_type = validated_data.get('flight_type', instance.flight_type)
+        instance.departure = validated_data.get('departure', instance.departure)
+        instance.destination = validated_data.get('destination', instance.destination)
+        instance.arrival_date= validated_data.get('arrival_date', instance.arrival_date)
+        instance.departure_date= validated_data.get('departure_date', instance.departure_date)
+        instance.save()
+        return instance
+    
+class RentalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rental
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Rental.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.itinerary_id = validated_data.get('itinerary_id', instance.itinerary_id)
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.rental_company = validated_data.get('rental_company', instance.rental_company)
+        instance.pick_up_location = validated_data.get('pick_up_location', instance.pick_up_location)
+        instance.return_location = validated_data.get('return_location', instance.return_location)
+        instance.pick_up_date= validated_data.get('pick_up_date', instance.pick_up_date)
+        instance.return_date= validated_data.get('return_date', instance.return_date)
+        instance.save()
+        return instance
+
+class AffinitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Affinity
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Affinity.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.itinerary_id = validated_data.get('itinerary_id', instance.itinerary_id)
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.affinity_type = validated_data.get('affinity_type', instance.affinity_type)
+        instance.save()
+        return instance
+    
+class SightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sight
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Sight.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.itinerary_id = validated_data.get('itinerary_id', instance.itinerary_id)
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.sight_name = validated_data.get('sight_name', instance.sight_name)
+        instance.save()
+        return instance
 
--- backend/moose_api/urls.py
+++ backend/moose_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import MyTokenObtainPairView
+from .views import * #MyTokenObtainPairView
 
 #from the simpleJWT authentication
 from rest_framework_simplejwt.views import (
@@ -8,8 +8,43 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', views.getRoutes),
+    path('', getRoutes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), #takes in the username and password to give an authentication and refresh token
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #takes in the refresh token -> gives a new refresh token
+    #view, create, update and delete itineraries
+    path('view-all-itineraries/', view_all_itineraries, name='view_all_itineraries'), 
+    path('view-itinerary/<int:pk>/', view_itinerary, name='view_itinerary'), 
+    path('create-itinerary/', create_itinerary, name='create_itinerary'), 
+    path('update-itinerary/<int:pk>/', update_itinerary, name='update_itinerary'), 
+    path('delete-itinerary/<int:pk>/', delete_itinerary, name='delete_itinerary'), 
+    # view, save, update and delete flight
+    path('view-all-flights/', view_all_flights, name='view_all_flights'), 
+    path('view-flight/<int:pk>/', view_flight, name='view_flight'), 
+    path('select-flight/', select_flight, name='select_flight'), 
+    path('update-flight/<int:pk>/', update_flight, name='update_flight'), 
+    path('delete-flight/<int:pk>/', delete_flight, name='delete_flight'), 
+    # save, update and delete hotel
+    path('view-all-hotels/', view_all_hotels, name='view_all_hotel'), 
+    path('view-hotel/<int:pk>/', view_hotel, name='view_hotel'), 
+    path('select-hotel/', select_hotel, name='select_hotel'), 
+    path('update-hotel/<int:pk>/', update_hotel, name='update_hotel'), 
+    path('delete-hotel/<int:pk>/', delete_hotel, name='delete_hotel'), 
+    # save, update and delete rental
+    path('view-all-rentals/', view_all_rentals, name='view_all_rentals'), 
+    path('view-rental/<int:pk>/', view_rental, name='view_rental'), 
+    path('select-rental/', select_rental, name='select_rental'), 
+    path('update-rental/<int:pk>/', update_rental, name='update_rental'), 
+    path('delete-rental/<int:pk>/', delete_rental, name='delete_rental'), 
+    # select, update and delete affinity - travel type
+    path('view-all-affinities/', view_all_affinities, name='view_all_affinities'), 
+    path('view-affinity/<int:pk>/', view_affinity, name='view_affinity'), 
+    path('select-affinity/', select_affinity, name='select_affinity'), 
+    path('update-affinity/<int:pk>/', update_affinity, name='update_affinity'), 
+    path('delete-affinity/<int:pk>/', delete_affinity, name='delete_affinity'), 
+    # select, update and delete sight
+    path('view-all-sights/', view_all_sights, name='view_all_sights'), 
+    path('view-sight/<int:pk>/', view_sight, name='view_sight'),
+    path('select-sight/', select_sight, name='select_sight'), 
+    path('update-sight/<int:pk>/', update_sight, name='update_sight'), 
+    path('delete-sight/<int:pk>/', delete_sight, name='delete_sight'), 
 ]
-
