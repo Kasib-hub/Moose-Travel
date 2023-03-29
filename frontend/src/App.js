4f10b8498c4c194cabb95e18190717b97a58fb6f
Austin Grabarkewitz <agrabark@gmail.com>
2023-03-28 21:12:18 -0400

    "got rid of FlightSearch test component"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css'
 import { HashRouter, Routes, Route, Link, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
 import Chatbot from './components/Chatbot';
-import FlightSearch from './components/FlightSearch';
 import HotelSearch from './components/HotelSearch';
 import SupportEngine from './components/SupportEngine/SupportEngine'
 import RoundTripSearchBar from './components/RoundTripSearchBar';
