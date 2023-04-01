57adc1f7196caafa10f55050f14d90a1fd66d419
Hui Zhao <huizhao1003@gmail.com>
2023-04-01 14:51:20 -0400

    delete simpleJWT

--- backend/moose_api/urls.py
+++ backend/moose_api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from .views import MyTokenObtainPairView
 from .views import *
-#from the simpleJWT authentication
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
