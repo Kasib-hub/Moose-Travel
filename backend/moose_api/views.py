061f01b2ed342452a728374798f6ce940efc1a69
Maddesea <maddesea@oregonstate.edu>
2023-04-04 18:46:00 -0500

    Fixed issues causing automated tests to fail - second attempt

--- backend/moose_api/views.py
+++ backend/moose_api/views.py
@@ -7,24 +7,19 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import *
 from .serializers import *
 
-
-
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
         # Add custom claims
         token['username'] = user.username #grabs the specified attributes of the user model. 'token' here is basically in encripted dictionary
         # ...
-
         return token
     
 class MyTokenObtainPairView(TokenObtainPairView):   #creates a new view for the serialized information that's based off the old template by inheriting the old method ('template')
--- frontend/src/api/Rental/Rental.js
+++ frontend/src/api/Rental/Rental.js
@@ -198,6 +198,109 @@ const deleteCar = async (token, itineraryID, carID) => {
     }
   };
 
+  const getAllRentalsByItinerary = async (token, itineraryID) => {
+    const url = `http://${BASE_URL}/api/itinerary/${itineraryID}/rental/`;
+    const context = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    };
+    const res = await fetch(url, context);
+    const body = await res.json();
+    if (res.status === 400) {
+      alert(`Error: ${JSON.stringify(body)}`);
+    } else if (!res.ok) {
+      alert(`${res.status} (${res.statusText})`);
+    } else {
+      return body;
+    }
+  };
+
+  const getRentalByID = async (token, itineraryID, rentalID) => {
+    const url = `http://${BASE_URL}/api/itinerary/${itineraryID}/rental/${rentalID}/`;
+    const context = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    };
+    const res = await fetch(url, context);
+    const body = await res.json();
+    if (res.status === 400) {
+      alert(`Error: ${JSON.stringify(body)}`);
+    } else if (!res.ok) {
+      alert(`${res.status} (${res.statusText})`);
+    } else {
+      return body;
+    }
+  };
+
+  const createRental = async (token, data, itineraryID) => {
+    const url = `http://${BASE_URL}/api/itinerary/${itineraryID}/rental/`;
+    const context = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    };
+    const res = await fetch(url, context);
+    const body = await res.json();
+    if (res.status === 400) {
+      alert(`Error: ${JSON.stringify(body)}`);
+    } else if (!res.ok) {
+      alert(`${res.status} (${res.statusText})`);
+    } else {
+      return body;
+    }
+  };
+
+  const editRental = async (token, data, itineraryID, rentalID) => {
+    const url = `http://${BASE_URL}/api/itinerary/${itineraryID}/rental/${rentalID}/`;
+    const context = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    };
+    const res = await fetch(url, context);
+    const body = await res.json();
+    if (res.status === 400) {
+      alert(`Error: ${JSON.stringify(body)}`);
+    } else if (!res.ok) {
+      alert(`${res.status} (${res.statusText})`);
+    } else {
+      return body;
+    }
+  };
+  
+  const deleteRental = async (token, itineraryID, rentalID) => {
+    const url = `http://${BASE_URL}/api/itinerary/${itineraryID}/rental/${rentalID}/`;
+    const context = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    };
+    const res = await fetch(url, context);
+    const body = await res.json();
+    if (res.status === 400) {
+      alert(`Error: ${JSON.stringify(body)}`);
+    } else if (!res.ok) {
+      alert(`${res.status} (${res.statusText})`);
+    } else {     
+      return body;
+    }
+  };
+  
+
   export {
     // Car functions
     getAllCarsByItinerary,
@@ -212,4 +315,12 @@ const deleteCar = async (token, itineraryID, carID) => {
     createReservation,
     editReservation,
     deleteReservation,
+  
+    // Rental functions
+    getAllRentalsByItinerary,
+    getRentalByID,
+    createRental,
+    editRental,
+    deleteRental,
   };
+  
