7e1a8995f33a142309fe7deb259a7ae114a48fc6
Austin Grabarkewitz <agrabark@gmail.com>
2023-04-06 19:56:13 -0400

    correct App.js I hope

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import NavBar from "./components/NavBar/NavBar";
-// import { Navigate } from 'react-router-dom';
-// import MapsPage from "./pages/MapsPage";
-// import ApiExample from "./pages/ApiExample";
-// import EditPersonalInfo from "./pages/EditPersonalInfo";
-import { useState } from "react";
-// import YourItineriesPage from "./pages/YourItineriesPage";
-import TripSelectionPage from "./pages/TripSelectionPage";
-import ChooseFlightPage from "./pages/ChooseFlightPage";
-import ChooseHotelPage from "./pages/ChooseHotelPage";
-import ChooseRestaurantPage from "./pages/ChooseRestaurantPage";
-import ChooseActivityPage from "./pages/ChooseActivityPage";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import TripSummaryPage from "./pages/TripSummaryPage";
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoutes from './utils/PrivateRoutes';
+import { AuthProvider } from './context/AuthContext';
+import NavBar from './components/NavBar/NavBar';
+import StartItineraryPage from './pages/StartItineraryPage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+// import { useLoadScript } from '@react-google-maps/api';
+import EditPersonalInfo from './pages/EditPersonalInfo';
+import { useState } from 'react';
+import YourItinerariesPage from './pages/YourItineriesPage';
+import TripSelectionPage from './pages/TripSelectionPage';
+import ChooseFlightPage from './pages/ChooseFlightPage';
+import ChooseHotelPage from './pages/ChooseHotelPage'
+import ChooseActivityPage from './pages/ChooseActivityPage'
+import ChooseRestaurantPage from './pages/ChooseRestaurantPage'
+import RentalCarPage from './pages/RentalCarPage'
+import ChooseGenrePage from './pages/ChooseGenrePage';
+import TripSummaryPage from './pages/TripSummaryPage'
 
 function App() {
-  const [selections, setSelections] = useState([]);
+  // loading the google maps script
+  // const libraries = ['places']
+
+  // const { isLoaded } = useLoadScript({
+  //   googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+  //   libraries
+  // })
+
+  // the selections user makes on what forms they want to see when building their trip
+  const [selections, setSelections] = useState([])
+  // the genres the user likes
+  const [likes, setLikes] = useState([])
+
+
+
+  if (!isLoaded) return <h2>Loading...</h2>
 
   return (
     <div className="App">
@@ -29,6 +46,7 @@ function App() {
             <Route element={<PrivateRoutes />}>
               <Route path="/" element={<StartItineraryPage />} />
               <Route path="/itinerary/:userID/your-itineraries" element={<YourItinerariesPage />} />
+              <Route path="/itinerary/:userID/user-information" element={<EditPersonalInfo />} />
               <Route path="/itinerary/:itineraryID/trip-selection" element={<TripSelectionPage selections={selections} setSelections={setSelections}/>} />
               <Route path="/itinerary/:itineraryID/choose-flight" element={<ChooseFlightPage selections={selections} setSelections={setSelections}/>} />
               <Route path="/itinerary/:itineraryID/choose-hotel" element={<ChooseHotelPage selections={selections} setSelections={setSelections}/>} />
@@ -38,11 +56,11 @@ function App() {
               <Route path="/itinerary/:itineraryID/choose-genre" element={<ChooseGenrePage likes={likes} setLikes={setLikes}/>} />
               {/* Trip summary makes the get requests*/}
               <Route path="/itinerary/:itineraryID/trip-summary" element={<TripSummaryPage />} /> 
-              <Route path="/itinerary/:itineraryID/gpt" element={<ChatGPTSummaryRequest likes={likes}/>}/>
             </Route>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
           </Routes>
+          {/* <SupportEngine /> */}
         </AuthProvider>
       </Router>
     </div>
